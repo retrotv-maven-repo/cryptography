ad9013b8680f3d87591e1a8273a091f3c6c4c15a
yjj8353 <yjj8353@gmail.com>
2023-02-22 22:52:03 +0900

    주석 추가

--- src/main/java/dev/retrotv/crypt/OneWayEncryption.java
+++ src/main/java/dev/retrotv/crypt/OneWayEncryption.java
@@ -6,6 +6,13 @@ import java.util.Base64;
 
 public interface OneWayEncryption {
 
+    /**
+     * 문자열 데이터를 암호화 하고, 암호화 된 문자열을 반환 받습니다.
+     * 이 때, 암호화 된 데이터는 지정한 {@link Encode} 타입의 문자열로 인코딩 됩니다.
+     * @param text 암호화 할 문자열
+     * @param encode byte[] 데이터를 문자열로 인코딩 할 때 사용할 {@link Encode} 유형
+     * @return 암호화 된 문자열
+     */
     default String encrypt(String text, Encode encode) {
         byte[] data = encrypt(text.getBytes(StandardCharsets.UTF_8));
 
@@ -15,19 +22,48 @@ public interface OneWayEncryption {
             case BASE64:
                 return new String(Base64.getEncoder().encode(encrypt(data))).toLowerCase();
         }
+
         return null;
     }
 
+    /**
+     * 데이터를 암호화 하고, 암호화 된 데이터를 반환 받습니다.
+     * @param data 암호화 할 데이터
+     * @return 암호화 된 데이터
+     */
     byte[] encrypt(byte[] data);
 
+    /**
+     * 추가 문자열이 첨가 된 문자열 데이터를 암호화 하고, 암호화 된 문자열을 반환 받습니다.
+     * 이 때, 암호화 된 데이터는 지정한 {@link Encode} 타입의 문자열로 인코딩 됩니다.
+     * @param text 암호화 할 문자열
+     * @param salt 암호화 할 문자열에 첨가할 추가 문자열
+     * @param encode byte[] 데이터를 문자열로 인코딩 할 때 사용할 {@link Encode} 유형
+     * @return 암호화 된 문자열
+     */
     default String encrypt(String text, String salt, Encode encode) {
         return encrypt(text.concat(salt), encode);
     }
 
+    /**
+     * 암호화 되지 않은 문자열을 암호화 하고, 기존에 암호화 된 문자열과 비교하열 일치 여부를 반환합니다.
+     * @param text 암호화 할 문자열
+     * @param encode byte[] 데이터를 문자열로 인코딩 할 때 사용할 {@link Encode} 유형
+     * @param encryptedText 비교할 암호화 된 문자열
+     * @return 일치 여부
+     */
     default boolean matches(String text, Encode encode, String encryptedText) {
         return encryptedText.equals(encrypt(text, encode));
     }
 
+    /**
+     * 추가 문자열을 추가한 암호화 되지 않은 문자열을 암호화 하고, 기존에 암호화 된 문자열과 비교하여 일치 여부를 반환합니다.
+     * @param text 암호화 할 문자열
+     * @param salt 암호화 할 문자열에 첨가할 추가 문자열
+     * @param encode byte[] 데이터를 문자열로 인코딩 할 때 사용할 {@link Encode} 유형
+     * @param encryptedText 비교할 암호화 된 문자열
+     * @return 일치 여부
+     */
     default boolean matches(String text, String salt, Encode encode, String encryptedText) {
         return matches(text.concat(salt), encode, encryptedText);
     }
--- src/main/java/dev/retrotv/crypt/TwoWayEncryption.java
+++ src/main/java/dev/retrotv/crypt/TwoWayEncryption.java
@@ -5,19 +5,49 @@ import java.util.Base64;
 
 public interface TwoWayEncryption {
 
+    /**
+     * 문자열을 암호화 하고, 암호화 된 문자열을 반환 받습니다.
+     * 이 때, 암호화 된 데이터는 {@link Base64} 타입의 문자열로 인코딩 됩니다.
+     * @param text 암호화 할 문자열
+     * @param key 암호화 시, 사용할 키
+     * @return 암호화 된 문자열
+     */
     default String encrypt(String text, String key) {
         byte[] data = text.getBytes(StandardCharsets.UTF_8);
         return new String(Base64.getEncoder().encode(encrypt(data, key)));
     }
 
+    /**
+     * 데이터를 암호화 하고, 암호화 된 데이터를 반환 받습니다.
+     * @param data 암호화 할 데이터
+     * @param key 암호화 시, 사용할 키
+     * @return 암호화 된 데이터
+     */
     byte[] encrypt(byte[] data, String key);
 
+    /**
+     * 암호화 된 문자열을 복호화 하고, 복호화 된 문자열을 반환 받습니다.
+     * 복호화 할 문자열은 {@link Base64} 유형으로 인코딩 된 문자열이어야 합니다.
+     * @param encryptedText 암호화 된 문자열
+     * @param key 복호화 시, 사용할 키
+     * @return 복호화 된 문자열
+     */
     default String decrypt(String encryptedText, String key) {
         byte[] data = Base64.getDecoder().decode(encryptedText.getBytes(StandardCharsets.UTF_8));
         return new String(decrypt(data, key));
     }
 
+    /**
+     * 암호화 된 데이터를 복호화 하고, 복호화 된 데이터를 반환 받습니다.
+     * @param encryptedData 암호화 된 데이터
+     * @param key 복호화 시, 사용할 키
+     * @return 복호화 된 데이터
+     */
     byte[] decrypt(byte[] encryptedData, String key);
 
+    /**
+     * 암복호화 시, 사용할 키를 생성합니다.
+     * @return 생성된 키
+     */
     String generateKey();
 }
