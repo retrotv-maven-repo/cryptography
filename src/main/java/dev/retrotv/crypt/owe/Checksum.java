f4a1e1c60f1d2e77ffd254d10d4129a44dd3a8e0
yjj8353 <yjj8353@gmail.com>
2023-03-28 01:05:08 +0900

    주석 내용 추가

--- src/main/java/dev/retrotv/crypt/owe/Checksum.java
+++ src/main/java/dev/retrotv/crypt/owe/Checksum.java
@@ -8,17 +8,37 @@ import java.nio.file.Files;
 public interface Checksum {
 
     /**
+     * data를 해시해 checksum을 생성하고 반환합니다.
      *
-     * @param data
-     * @return
+     * @param data 해시 할 데이터
+     * @return 체크섬
      */
     String encode(byte[] data);
 
     /**
+     * file을 해시해 checksum을 생성하고 반환합니다.
      *
-     * @param data
-     * @param checksum
-     * @return
+     * @param file 해시 할 파일
+     * @return 체크섬
+     * @throws IOException 파일을 읽어들이는 과정에서 오류가 발생할 경우 던짐
+     */
+    default String encode(File file) throws IOException {
+        try (DataInputStream dis = new DataInputStream(Files.newInputStream(file.toPath()))) {
+            byte[] fileData = new byte[(int) file.length()];
+            dis.readFully(fileData);
+
+            return encode(fileData);
+        } catch (IOException e) {
+            throw new IOException("파일을 읽어들이는 과정에서 오류가 발생했습니다.", e);
+        }
+    }
+
+    /**
+     * data를 해시해 체크섬을 생성한 뒤, 비교할 checksum과 일치하는지 확인하고 일치 여부를 반환합니다.
+     *
+     * @param data 해시 할 데이터
+     * @param checksum 비교할 체크섬
+     * @return 일치 여부
      */
     default boolean matches(byte[] data, String checksum) {
         if (data == null || checksum == null) {
@@ -30,10 +50,10 @@ public interface Checksum {
 
     /**
      *
-     * @param file
-     * @param checksum
-     * @return
-     * @throws IOException
+     * @param file 해시 할 파일
+     * @param checksum 비교할 체크섬
+     * @return 일치 여부
+     * @throws IOException 파일을 읽어들이는 과정에서 오류가 발생할 경우 던짐
      */
     default boolean matches(File file, String checksum) throws IOException {
         if (file == null || checksum == null) {
@@ -46,12 +66,18 @@ public interface Checksum {
             fileData = new byte[(int) file.length()];
             dis.readFully(fileData);
         } catch (IOException e) {
-            throw new IOException("파일을 읽어들이는 과정에서 예상치 못한 오류가 발생했습니다.");
+            throw new IOException("파일을 읽어들이는 과정에서 오류가 발생했습니다.");
         }
 
         return matches(fileData, checksum);
     }
 
+    /**
+     *
+     * @param data1 해시 할 데이터
+     * @param data2 해시 할 데이터
+     * @return 일치 여부
+     */
     default boolean matches(byte[] data1, byte[] data2) {
         if (data1 == null || data2 == null) {
             throw new NullPointerException("비교할 데이터가 null 입니다.");
@@ -60,6 +86,13 @@ public interface Checksum {
         return encode(data1).equals(encode(data2));
     }
 
+    /**
+     *
+     * @param file1 해시 할 파일
+     * @param file2 해시 할 파일
+     * @return 일치 여부
+     * @throws IOException 파일을 읽어들이는 과정에서 오류가 발생할 경우 던짐
+     */
     default boolean matches(File file1, File file2) throws IOException {
         if (file1 == null || file2 == null) {
             throw new NullPointerException("비교할 파일이 null 입니다.");
@@ -72,14 +105,14 @@ public interface Checksum {
             file1Data = new byte[(int) file1.length()];
             dis.readFully(file1Data);
         } catch (IOException e) {
-            throw new IOException("파일을 읽어들이는 과정에서 예상치 못한 오류가 발생했습니다.");
+            throw new IOException("파일을 읽어들이는 과정에서 오류가 발생했습니다.");
         }
 
         try (DataInputStream dis = new DataInputStream(Files.newInputStream(file2.toPath()))) {
             file2Data = new byte[(int) file2.length()];
             dis.readFully(file2Data);
         } catch (IOException e) {
-            throw new IOException("파일을 읽어들이는 과정에서 예상치 못한 오류가 발생했습니다.");
+            throw new IOException("파일을 읽어들이는 과정에서 오류가 발생했습니다.");
         }
 
         return matches(file1Data, file2Data);
