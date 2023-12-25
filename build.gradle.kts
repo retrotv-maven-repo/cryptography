58819b0d9b987db1271408ec595a140d0487d0e9
yjj8353 <yjj8353@gmail.com>
2023-12-25 16:50:35 +0900

    data-utils 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     api("commons-codec:commons-codec:1.16.0")
     api("org.springframework.security:spring-security-core:5.8.9")
-    implementation("com.github.retrotv-maven-repo:data-utils:0.13.2-alpha")
+    implementation("com.github.retrotv-maven-repo:data-utils:0.13.3-alpha")
     implementation("com.github.retrotv-maven-repo:random-value:0.5.0-alpha")
 
     // Argon2, SCrypt java.lang.NoClassDefFoundError 방지용
