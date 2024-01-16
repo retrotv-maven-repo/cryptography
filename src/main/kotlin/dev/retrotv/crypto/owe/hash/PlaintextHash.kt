e910e378fbbe11d432cf605cefc219b0c47629a9
yjj8353 <yjj8353@gmail.com>
2024-01-16 23:35:57 +0900

    주석 추가

--- src/main/kotlin/dev/retrotv/crypto/owe/hash/PlaintextHash.kt
+++ src/main/kotlin/dev/retrotv/crypto/owe/hash/PlaintextHash.kt
@@ -10,6 +10,17 @@ interface PlaintextHash : BinaryHash {
     }
 
     fun hash(plaintext: CharSequence, charset: Charset): String {
+
+        /*
+         * // 좀 더, 확실하게 구현하고 싶은 경우, ByteBuffer를 이용해 ByteArray(byte[])로 변환할 것!
+         * val byteBuffer = Charset.forName(charset.name())
+         *                         .encode(CharBuffer.wrap(plaintext))
+         * val result = ByteArray(byteBuffer.remaining())
+         * byteBuffer[result]
+         *
+         * return hash(result)
+         */
+
         return hash(plaintext.toString()
                              .toByteArray(charset))
     }
--- src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3256.kt
+++ src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3256.kt
@@ -3,6 +3,12 @@ package dev.retrotv.crypto.owe.hash.sha
 import dev.retrotv.crypto.owe.hash.Hash
 import dev.retrotv.enums.Algorithm.Hash.SHA3256
 
+/**
+ * SHA3-256 알고리즘으로 암호화 하기 위한 [Hash] 추상 클래스의 구현체 입니다.
+ *
+ * @author  yjj8353
+ * @since   1.0.0
+ */
 class SHA3256 : Hash() {
 
     init {
