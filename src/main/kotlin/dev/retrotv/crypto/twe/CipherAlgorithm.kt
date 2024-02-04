de4fe1b795900fb5f3e84cac144a06a75a947553
yjj8353 <yjj8353@gmail.com>
2024-02-04 17:03:16 +0900

    BCKeyGenerator -> KeyGenerator로 이름 변경

--- src/main/kotlin/dev/retrotv/crypto/twe/CipherAlgorithm.kt
+++ src/main/kotlin/dev/retrotv/crypto/twe/CipherAlgorithm.kt
@@ -3,7 +3,7 @@ package dev.retrotv.crypto.twe
 import dev.retrotv.enums.Algorithm
 import org.bouncycastle.crypto.BlockCipher
 
-abstract class CipherAlgorithm : BCKeyGenerator {
+abstract class CipherAlgorithm : KeyGenerator {
     lateinit var engine: BlockCipher
     lateinit var algorithm: Algorithm.Cipher
 }
--- src/main/kotlin/dev/retrotv/crypto/twe/KeyGenerator.kt
+++ src/main/kotlin/dev/retrotv/crypto/twe/KeyGenerator.kt
@@ -6,7 +6,7 @@ package dev.retrotv.crypto.twe
  * @author  yjj8353
  * @since   1.0.0
  */
-fun interface BCKeyGenerator {
+fun interface KeyGenerator {
 
     /**
      * 암복호화 시, 사용될 키를 생성하고 반환합니다.
