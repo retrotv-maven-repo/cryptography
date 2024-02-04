e38dc11c4c711d69829b1ed36a927e903cf60dca
yjj8353 <yjj8353@gmail.com>
2024-02-04 17:04:38 +0900

    import 최적화

--- src/main/kotlin/dev/retrotv/crypto/twe/des/TripleDES.kt
+++ src/main/kotlin/dev/retrotv/crypto/twe/des/TripleDES.kt
@@ -1,17 +1,15 @@
 package dev.retrotv.crypto.twe.des
 
-import dev.retrotv.crypto.twe.BCKeyGenerator
 import dev.retrotv.crypto.twe.CipherAlgorithm
 import dev.retrotv.enums.Algorithm
 
 import org.bouncycastle.crypto.KeyGenerationParameters
-import org.bouncycastle.crypto.engines.DESEngine
 import org.bouncycastle.crypto.engines.DESedeEngine
 import org.bouncycastle.crypto.generators.DESedeKeyGenerator
 import java.security.SecureRandom
 
 @Deprecated("해킹에 취약한 양방향 암호화 알고리즘 입니다.")
-class TripleDES : CipherAlgorithm(), BCKeyGenerator {
+class TripleDES : CipherAlgorithm() {
 
     init {
         this.engine = DESedeEngine()
