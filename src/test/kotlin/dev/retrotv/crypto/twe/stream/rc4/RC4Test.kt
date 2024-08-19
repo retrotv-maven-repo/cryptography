3ffb10be1f6b1a57088847c21bb4da25c012f261
yjj8353 <yjj8353@gmail.com>
2024-08-19 17:57:49 +0900

    RC4 테스트 케이스 추가

--- src/test/kotlin/dev/retrotv/crypto/twe/stream/rc4/RC4Test.kt
+++ src/test/kotlin/dev/retrotv/crypto/twe/stream/rc4/RC4Test.kt
@@ -3,7 +3,9 @@ package dev.retrotv.crypto.twe.stream.rc4
 import dev.retrotv.crypto.twe.algorithm.stream.RC4
 import dev.retrotv.data.utils.ByteUtils
 import org.junit.jupiter.api.DisplayName
+import java.io.ByteArrayOutputStream
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class RC4Test {
 
@@ -14,10 +16,21 @@ class RC4Test {
         val key = "0123456789012345".toByteArray()
 
         val rc4 = RC4()
-        val encryptedData = rc4.encrypt(message, key)
+        var encryptedData = rc4.encrypt(message, key)
         println(ByteUtils.toHexString(encryptedData))
 
         val originalData = rc4.decrypt(encryptedData, key)
         println(String(originalData))
+
+        val inputStream = message.inputStream()
+        val outputStream = ByteArrayOutputStream()
+        rc4.encrypt(inputStream, outputStream, key)
+
+        encryptedData = outputStream.toByteArray()
+        println(ByteUtils.toHexString(encryptedData))
+
+        outputStream.reset()
+        rc4.decrypt(encryptedData.inputStream(), outputStream, key)
+        assertEquals(String(outputStream.toByteArray()), String(originalData))
     }
 }
