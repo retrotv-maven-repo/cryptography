a3c193d2a3d91f7ea6bd75650b2ad8c1ed3203b1
yjj8353 <yjj8353@gmail.com>
2024-08-19 13:17:54 +0900

    src/main/java의 패키지들은 소나에서 인식하지 않도록 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,6 +104,6 @@ sonar {
         property("sonar.projectKey", "retrotv-maven-repo_cryptography")
         property("sonar.organization", "retrotv-maven-repo")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.exclusions", "**/exception/*,**/enums/*")
+        property("sonar.coverage.exclusions", "**/exception/*,**/enums/*,src/main/java/**")
     }
 }
