90b3c807234f57a2d0edf1adf22b3abe17c08ffd
yjj8353 <yjj8353@gmail.com>
2024-08-27 13:08:32 +0900

    테스트 코드 추가

--- cryptography-hash/src/test/kotlin/dev/retrotv/crypto/hash/HashTest.kt
+++ cryptography-hash/src/test/kotlin/dev/retrotv/crypto/hash/HashTest.kt
@@ -109,12 +109,14 @@ class HashTest {
     private fun passwordHashTest(algorithm: EHash) {
         val h = Hash.newInstance(algorithm)
         assertTrue(h.matches(password.toByteArray(), getHash(algorithm)))
+        assertEquals(h.hash(password), getHash(algorithm))
         assertEquals(h.hash(password, Charsets.UTF_8), getHash(algorithm))
     }
 
     private fun fileHashTest(algorithm: EHash) {
         val h = Hash.newInstance(algorithm)
         assertTrue(h.matches(File(resource?.file ?: ""), h.hash(File(resource?.file ?: ""))))
+        assertFalse(h.matches(File(resource?.file ?: ""), null))
     }
 
     @Throws(IOException::class)
