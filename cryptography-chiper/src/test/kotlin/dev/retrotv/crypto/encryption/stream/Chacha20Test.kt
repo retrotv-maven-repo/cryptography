da7f66dcf7b9cc2cdeaa2e87f6a364ea1ece4135
yjj8353 <yjj8353@gmail.com>
2024-08-28 15:29:16 +0900

    stream 암호화 테스트 추가

---
+++ cryptography-chiper/src/test/kotlin/dev/retrotv/crypto/encryption/stream/Chacha20Test.kt
@@ -0,0 +1,24 @@
+package dev.retrotv.crypto.encryption.stream
+
+import dev.retrotv.crypto.encryption.generator.generateIV
+import dev.retrotv.crypto.encryption.generator.generateKey
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Chacha20Test {
+    private val plainText = "The quick brown fox jumps over the lazy dog"
+
+    @Test
+    @DisplayName("Chacha20 암호화 테스트")
+    fun test_chacha20() {
+        val chacha20 = Chacha20()
+        val key = generateKey(32)
+        val iv = generateIV(8)
+
+        val encrypted = chacha20.encrypt(plainText.toByteArray(), key, iv)
+        val decrypted = chacha20.decrypt(encrypted, key, iv)
+
+        assertEquals(plainText, String(decrypted))
+    }
+}
