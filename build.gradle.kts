096f0ed4bad07967f566dc144c8d9b7ab2f67e77
yjj8353 <yjj8353@gmail.com>
2024-09-09 17:08:24 +0900

    버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "dev.retrotv"
-version = "0.42.7-alpha"
+version = "0.42.8-alpha"
 
 // Github Action 버전 출력용
 tasks.register("printVersionName") {
@@ -80,6 +80,11 @@ subprojects {
         testImplementation(kotlin("test"))
         testImplementation("org.junit.jupiter:junit-jupiter-params:${junit}")
         testImplementation("org.json:json:${json}")
+    }
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
     }
 
     publishing {
