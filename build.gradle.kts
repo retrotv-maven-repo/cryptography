259ab677def42da075895fac1eacdf8d3f55598e
yjj8353 <yjj8353@gmail.com>
2024-09-09 11:32:00 +0900

    버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "dev.retrotv"
-version = "0.42.5-alpha"
+version = "0.42.6-alpha"
 
 // Github Action 버전 출력용
 tasks.register("printVersionName") {
@@ -28,7 +28,7 @@ tasks {
     }
 }
 
-afterEvaluate {
+publishing {
     publishing {
         publications {
             create<MavenPublication>("maven") {
