94dc510cb26cd72df3996b5687b8af00451119d1
yjj8353 <yjj8353@gmail.com>
2025-01-09 17:01:46 +0900

    gradlew.kts 수정

--- cryptography-cipher/build.gradle.kts
+++ cryptography-cipher/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm")
 }
 
+// kr.re 패키지는 제외하고 빌드
 sourceSets {
     main {
         java {
--- cryptography-password/build.gradle.kts
+++ cryptography-password/build.gradle.kts
@@ -2,15 +2,6 @@ plugins {
     kotlin("jvm")
 }
 
-// org.springframework.security.crypto 패키지를 제외하고 빌드
-sourceSets {
-    main {
-        java {
-            exclude("org/springframework/security/crypto/**")
-        }
-    }
-}
-
 dependencies {
     implementation(project(":cryptography-core"))
 }
