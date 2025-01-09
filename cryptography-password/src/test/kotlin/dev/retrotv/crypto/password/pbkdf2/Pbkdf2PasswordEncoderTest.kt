ecc46d074a49eca3f94c9ba83ea8df82ea11b10e
yjj8353 <yjj8353@gmail.com>
2025-01-09 12:46:52 +0900

    알고리즘 케이스별로 추가

--- cryptography-password/src/test/kotlin/dev/retrotv/crypto/password/pbkdf2/Pbkdf2PasswordEncoderTest.kt
+++ cryptography-password/src/test/kotlin/dev/retrotv/crypto/password/pbkdf2/Pbkdf2PasswordEncoderTest.kt
@@ -1,11 +1,11 @@
 package dev.retrotv.crypto.password.pbkdf2
 
-import dev.retrotv.crypto.password.enums.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import dev.retrotv.crypto.password.enums.SecretKeyFactoryAlgorithm.*
 
 class Pbkdf2PasswordEncoderTest {
 
@@ -22,8 +22,30 @@ class Pbkdf2PasswordEncoderTest {
 
     @Test
     @DisplayName("Pbkdf2PasswordEncoder(secret, saltLength, iterations, secretKeyFactoryAlgorithm) 테스트")
-    fun test_pbkdf2PasswordEncoder_secret_saltLength_iterations_secretKeyFactoryAlgorithm() {
+    fun test_pbkdf2PasswordEncoder1() {
+        val passwordEncoder = Pbkdf2PasswordEncoder("", 16, 310000, PBKDF2WithHmacSHA1)
+        val rawPassword = "password"
+        val encodedPassword = passwordEncoder.encode(rawPassword)
+
+        assertNotEquals(rawPassword, encodedPassword)
+        assertTrue(passwordEncoder.matches(rawPassword, encodedPassword))
+    }
+
+    @Test
+    @DisplayName("Pbkdf2PasswordEncoder(secret, saltLength, iterations, secretKeyFactoryAlgorithm) 테스트")
+    fun test_pbkdf2PasswordEncoder2() {
         val passwordEncoder = Pbkdf2PasswordEncoder("", 16, 310000, PBKDF2WithHmacSHA256)
+        val rawPassword = "password"
+        val encodedPassword = passwordEncoder.encode(rawPassword)
+
+        assertNotEquals(rawPassword, encodedPassword)
+        assertTrue(passwordEncoder.matches(rawPassword, encodedPassword))
+    }
+
+    @Test
+    @DisplayName("Pbkdf2PasswordEncoder(secret, saltLength, iterations, secretKeyFactoryAlgorithm) 테스트")
+    fun test_pbkdf2PasswordEncoder3() {
+        val passwordEncoder = Pbkdf2PasswordEncoder("", 16, 310000, PBKDF2WithHmacSHA512)
         val rawPassword = "password"
         val encodedPassword = passwordEncoder.encode(rawPassword)
 
