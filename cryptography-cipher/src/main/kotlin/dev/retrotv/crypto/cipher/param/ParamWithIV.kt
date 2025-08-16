a2bfa805551ac1ef9d79cafcd317bd3e525db52b
yjj8353 <yjj8353@gmail.com>
2025-08-16 09:18:01 +0900

    오탈자 수정

--- cryptography-cipher/src/main/kotlin/dev/retrotv/crypto/cipher/param/ParamWithIV.kt
+++ cryptography-cipher/src/main/kotlin/dev/retrotv/crypto/cipher/param/ParamWithIV.kt
@@ -2,7 +2,7 @@ package dev.retrotv.crypto.cipher.param
 
 /**
  * 암호화 및 복호화에 필요한 파라미터 클래스 입니다.
- * Params 클래스를 상속받습니다.
+ * Param 클래스를 상속받습니다.
  */
 class ParamWithIV(
     override val key: ByteArray,
