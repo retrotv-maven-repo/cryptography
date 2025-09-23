6cc0ffa2904a8ce7ccf783aacbac38f9d6d7951e
yjj8353 <yjj8353@gmail.com>
2025-09-23 20:15:42 +0900

    범위 변경

--- cryptography-cipher/src/test/java/dev/retrotv/crypto/cipher/stream/RC4Test.java
+++ cryptography-cipher/src/test/java/dev/retrotv/crypto/cipher/stream/RC4Test.java
@@ -33,6 +33,7 @@ class RC4Test {
     @Test
     @DisplayName("RC4 스트림 암호화 테스트")
     void testEncryptAndDecryptInputStream() {
+        
         // 준비
         byte[] key = "testkey123456789".getBytes(); // RC4는 16바이트까지 권장
         Param param = new Param(key);
