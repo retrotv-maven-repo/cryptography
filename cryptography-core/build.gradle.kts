cb2993d8bcf002c1733368d9bdc6a1c13dda22e1
yjj8353 <yjj8353@gmail.com>
2025-09-25 17:50:12 +0900

    api -> implementation으로 변경

--- cryptography-core/build.gradle.kts
+++ cryptography-core/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     java
-    `java-library`
 }
 
 val apacheCommonCodec = "1.18.0"
 val randomValue = "1.1.3"
 
 dependencies {
-    api("commons-codec:commons-codec:${apacheCommonCodec}")
+    implementation("commons-codec:commons-codec:${apacheCommonCodec}")
     implementation("dev.retrotv:random-value:${randomValue}")
 }
